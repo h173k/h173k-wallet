85b207df4e75112fad2717ba65a49ede70aae22f
h173k <h173k@hotmail.com>
2026-06-15 19:14:21 +0300

    Incorrect MAD statuses correction 3.1

--- src/App.jsx
+++ src/App.jsx
@@ -3816,7 +3816,7 @@ function SettingsView({ connection, publicKey, solBalance, onBack, showToast, on
         )}
       </div>
 
-      <div className="settings-section"><h3>{t('settings.about')}</h3><div className="settings-item"><span>{t('settings.version')}</span><span>1.5.2.0</span></div></div>
+      <div className="settings-section"><h3>{t('settings.about')}</h3><div className="settings-item"><span>{t('settings.version')}</span><span>1.5.2.1</span></div></div>
     </div>
   )
 }
